e5d8e2cb63f27ce7d488be2cb6f22caa80cac832
HuyVietDoan <huyvietdoan2003@gmail.com>
2024-11-21 01:09:06 +0700

    update project

--- js/main.js
+++ js/main.js
@@ -160,4 +160,20 @@ document.getElementById("btn-right").addEventListener("click", function() {
     }
 });
 
+function tai_lai_trang(){
+    location.reload();
+}
 
+const listBtn = document.querySelector('.js-btn_list')
+  const modal = document.querySelector('.modal') //luu class modal vao bien modal
+  const ClosetBtn = document.querySelector('.js-btn-close')
+  function showList() {
+    modal.classList.add('open')
+  }
+
+  function hideList() {
+    modal.classList.remove('open')
+  }
+
+  listBtn.addEventListener('click', showList)
+  ClosetBtn.addEventListener('click', hideList)
